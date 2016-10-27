3ed7609482d4d47ae5e492530e75fa6b04c556d3
fengshenzhu <fengshenzhu@gmail.com>
2016-10-27 17:32:19 +0800

    add hexo-deployer-git

--- blog_clone.sh
+++ blog_clone.sh
@@ -6,6 +6,7 @@ git checkout source
 npm install hexo --save
 hexo init temp
 mv temp/node_modules .
+hexo install hexo-deployer-git --save
 sleep 3
 rm -rf temp
 git clone https://github.com/iissnan/hexo-theme-next themes/next
