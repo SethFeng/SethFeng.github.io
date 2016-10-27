99d4dceb6520ba6db45760f13182feb9f7acd303
fengshenzhu <fengshenzhu@gmail.com>
2016-10-27 17:24:40 +0800

    update blog_clone.sh

--- blog_clone.sh
+++ blog_clone.sh
@@ -3,14 +3,11 @@
 # git clone git@github.com:SethFeng/sethfeng.github.io.git
 # cd sethfeng.github.io
 git checkout source
-npm install hexo-cli -g
+npm install hexo --save
 hexo init temp
 mv temp/node_modules .
-mv temp/package.json .
 sleep 3
 rm -rf temp
-npm install hexo-deployer-git --save
 git clone https://github.com/iissnan/hexo-theme-next themes/next
-hexo clean
 hexo g
 hexo s
