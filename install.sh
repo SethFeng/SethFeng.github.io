df951db26e417e553231cb08fba167b163a89999
fengshenzhu <fengshenzhu@gmail.com>
2017-05-12 10:56:17 +0800

    config

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 # git clone git@github.com:SethFeng/sethfeng.github.io.git
 # cd sethfeng.github.io
-git checkout source
+# git checkout source
 npm install hexo --save
 npm install hexo-server --save
 npm install hexo-deployer-git --save
 git clone https://github.com/iissnan/hexo-theme-next themes/next
-hexo g
-hexo s
+# hexo g
+# hexo s
