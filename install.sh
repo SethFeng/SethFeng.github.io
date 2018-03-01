6656289763b893d4066b3ca90a8452a3420d4cb8
fengshenzhu <fengshenzhu@gmail.com>
2018-03-01 18:03:47 +0800

    upgrade

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@
 # cd sethfeng.github.io
 # git checkout source
 # sh install.sh
-hexo init ../hexotemp
-cp -R ../hexotemp/node_module .
-cp ../hexotemp/package.json .
-rm -rf ../hexotemp
+# hexo init ../hexotemp
+# cp -R ../hexotemp/node_module .
+# cp ../hexotemp/package.json .
+# rm -rf ../hexotemp
 # npm install hexo --save
 # npm install hexo-server --save
-npm install hexo-deployer-git --save
-git clone https://github.com/iissnan/hexo-theme-next themes/next
+# npm install hexo-deployer-git --save
+git clone https://github.com/theme-next/hexo-theme-next themes/next
 # hexo g
 # hexo s
 # hexo d
